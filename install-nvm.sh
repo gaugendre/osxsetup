93420bac8f0a9611054477e74df84987be9f789b
Guilhem Augendre <guilhem.augendre@gmail.com>
2020-04-22 00:18:51 +0200

    install lts

--- install-nvm.sh
+++ install-nvm.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install lts/*
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
