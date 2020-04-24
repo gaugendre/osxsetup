6c133814c275645de1c75716bdf7c59490816300
Guilhem Augendre <guilhem.augendre@gmail.com>
2020-04-24 16:36:29 +0200

    fix default zsh home

--- install.sh
+++ install.sh
@@ -83,7 +83,7 @@ get_homebrew() {
 }
 
 get_ohmyzsh() {
-  [[ -d "$ZSH" ]] ||
+  [[ -d "$HOME/.oh-my-zsh" ]] ||
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
