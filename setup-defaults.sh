e034b758d60ba96b0e7d14e6f4c95da6a84ed7cd
Guilhem AUgendre <guilhem.augendre@gmail.com>
2020-07-07 14:38:15 +0200

    choppyless bluetooth

--- setup-defaults.sh
+++ setup-defaults.sh
@@ -21,6 +21,16 @@ defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
+# https://apple.stackexchange.com/questions/167245/yosemite-bluetooth-audio-is-choppy-skips
+# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 80 
+# defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 80 
+# defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 80 
+# defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" 80 
+# defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 80 
+# defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 80 
+# defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 80
+# sudo killall coreaudiod
+
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
