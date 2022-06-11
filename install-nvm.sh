42d6be2b3c8277eab1249d89848359e3f6aee977
Guilhem Augendre <guilhem.augendre@gmail.com>
2022-06-11 12:57:19 +0200

    update install scripts

--- install-nvm.sh
+++ install-nvm.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
--- install-rvm.sh
+++ install-rvm.sh
@@ -5,4 +5,4 @@ IFS=$'\n\t'
 gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 . ~/.rvm/scripts/rvm
-rvm rvmrc warning ignore allGemfiles
+# rvm rvmrc warning ignore allGemfiles
